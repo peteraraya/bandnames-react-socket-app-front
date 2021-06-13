7f2d24a783c908adb8903c6448246741ab387d53
Pedro Araya <Piteraraya@gmail.com>
2021-06-12 22:48:59 -0400

    Se realiza la creación de frontend de aplicación de bandas

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+
+## Instalaciones utilizadas 
+
+  * [socket.io-client](https://www.npmjs.com/package/socket.io-client)
+``` 
+npm i socket.io-client
+```
+
+
+
 ## Available Scripts
 
 In the project directory, you can run:
@@ -68,3 +78,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { BandAdd } from './components/BandAdd';
+import { BandList } from './components/BandList';
+import { io } from 'socket.io-client';
+
+const connectSocketServer = () =>{
+
+  const socket = io.connect('http://localhost:8080/',{
+    transports:['websocket']
+  });
+  return socket;
+}
 
 function App() {
+
+  const [socket] = useState( connectSocketServer() );
+  const [online, setOnline] = useState(false);
+  const [bands, setBands] = useState([]);
+
+  // Socket
+  useEffect(() => {
+    console.log(socket);
+    setOnline(socket.connected )
+  }, [socket]);
+
+  // Online = connected
+  useEffect(() => {   
+    socket.on('connect',()=>{
+        setOnline( true );
+    });
+  }, [socket]);
+
+
+  // Offline = disconnected
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
+
+  useEffect(() => {
+    
+    socket.on('current-bands', (bands)=>{
+        console.log(bands);
+        setBands(bands);
+    })
+
+  }, [socket]);
+
+ // Votar
+ const votar = (id) =>{
+  socket.emit('votar-banda', id );
+ }
+ // descontar votos
+ const descontar = (id) =>{
+  socket.emit('descontar-banda', id );
+ }
+
+ // Borrar banda
+ const borrar = ( id ) =>{
+   socket.emit('borrar-banda', id);
+ }
+
+ // Cambiar Nombre
+ const cambiarNombre = ( id, nombre ) => {
+    socket.emit('cambiar-nombre-banda', { id, nombre });
+ }
+
+ // Crear Banda
+ const crearBanda = (nombre) =>{
+  socket.emit('agregar-banda', { nombre });
+ }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container bg-dark text-light">
+
+      <div className="alert">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Services Status
+          {
+            online
+              ? <span className="text-success"> Online</span>
+              : <span className="text-danger"> Offline</span>
+          }
+        
+        
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+
+      <h1>Bandnames</h1>
+      <hr />
+
+      <div className="row mb-3">
+        <div className="col-8">
+          <BandList 
+            data={bands}
+            votar={votar}
+            descontar={descontar}
+            borrar={borrar}
+            cambiarNombre={cambiarNombre}
+          />
+          </div>
+        <div className="col-4">
+          <BandAdd 
+            crearBanda={crearBanda}
+          />
+        </div>
+      </div>
+
     </div>
   );
 }
